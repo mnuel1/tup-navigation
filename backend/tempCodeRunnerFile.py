7b98adf81c8644b82bb09dcec5c6a40b221dc65b
mnuel1 <mnuelmrin21@gmail.com>
2024-05-13 02:07:22 +0800

    added map data

--- backend/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-     current_node_index = self.node_to_index[current_node]
-        probabilities = self.calculate_probabilities(current_node_index, visited)
-        next_node_label = np.random.choice(list(probabilities.keys()), p=list(probabilities.values()))
-        return next_node_label
